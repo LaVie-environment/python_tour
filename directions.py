a04f11894d871b1a128e9e85d8864d9906ae3a30
Arm Bun <bellotaophyc@outlook.com>
2022-06-07 12:53:47 +0000

    a direction algorithm

---
+++ directions.py
@@ -0,0 +1,13 @@
+directions = [
+    'Head north on Broadway toward W 48th St',
+    'Turn left onto W 58th St',
+    'Turn right onto 8th Ave',
+    'Turn right onto Broadway',
+    'Turn left onto Lincoln Center Plaza',
+    'Turn right onto Jaffe Dr',
+    'Turn left onto Broadway',
+    'Turn left onto W 65th St'
+]
+
+for count, direction in enumerate(directions, start=1):
+    print(count, direction)
