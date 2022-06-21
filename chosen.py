5f6e72c69154df2fb5188e9fbb0daeee7dd994d0
Arm Bun <bellotaophyc@outlook.com>
2022-06-21 17:50:01 +0000

    649special

--- chosen.py
+++ chosen.py
@@ -73,3 +73,29 @@ isis = Cat('Isis')
 
 print(fido.speak())
 print(isis.speak())
+
+'''
+Special Methods
+'''
+
+class Book:
+    def __init__(self, title, author, pages):
+        print("A book is created")
+        self.title = title
+        self.author = author
+        self.pages = pages
+        
+    def __str__(self):
+        return "Title: %s, author: %s, pages: %s" %(self.title, self.author, self.pages)
+    def __len__(self):
+        return self.pages
+        
+    def __del__(self):
+        print("A book is destroyed")
+        
+book = Book("Python Rocks!", "Jose Portilla", 167)
+
+#Special Methods
+print(book)
+print(len(book))
+del book
