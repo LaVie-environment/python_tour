fdb2ccc64de476c5d1c12e62e65f691c9040d7c9
Taofeek <bellotaophyc@outlook.com>
2025-01-31 18:33:14 +0100

    remove the dictionary creation code to replace it with Athlete object creation code

--- code_magnets/extract.py
+++ code_magnets/extract.py
@@ -10,15 +10,22 @@ def sanitize(time_string):
     (mins, secs) = time_string.split(splitter)
     return(mins + '.' + secs)
 
+
+class Athlete:
+    def __init__(self, a _name, a_dob=None, a_times=[]):
+        self.name = a_name
+        self.dob = a_dob
+        self.times= a_times
+
+    def top3(self):
+        return(sorted(set([sanitize(t) for t in self.times]))[0:3])
+
 def get_coach_data(filename):
     try:
         with open(filename, 'r') as f:
             data = f.readline()
         templ = data.strip().split(',')
-        return({'Name': templ.pop(0),
-                'DOB': templ.pop(0),
-                'Times': str(sorted(set([sanitize(t) for t in templ]))[0:3])            
-        })
+        return(Athlete(templ.pop(0),templ.pop(0), templ))
     except IOError as ioerr:
         print('File error: ' + str(ioerr))
         return(None)
