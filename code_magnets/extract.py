7fe35344e1d88abdac5059044678ca1a6c1de22f
Taofeek <bellotaophyc@outlook.com>
2025-02-01 18:42:13 +0100

    cleanup the Athlete class for the AthleteList class

--- code_magnets/extract.py
+++ code_magnets/extract.py
@@ -10,7 +10,28 @@ def sanitize(time_string):
     (mins, secs) = time_string.split(splitter)
     return(mins + '.' + secs)
 
+class AthleteList(list):
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+            list.__init__([])
+            self.name = a_name
+            self.dob = a_dob
+            self.extend(a_times)
 
+    def top3(self):
+            return(sorted(set([sanitize(t) for t in self]))[0:3])
+
+
+def get_coach_data(filename):
+    try:
+        with open(filename, 'r') as f:
+            data = f.readline()
+        templ = data.strip().split(',')
+        return(AthleteList(templ.pop(0),templ.pop(0), templ))
+    except IOError as ioerr:
+        print('File error: ' + str(ioerr))
+        return(None)
+
+"""
 class Athlete:
     def __init__(self, a_name, a_dob=None, a_times=[]):
         self.name = a_name
@@ -19,11 +40,11 @@ class Athlete:
 
     def top3(self):
         return(sorted(set([sanitize(t) for t in self.times]))[0:3])
-    
+"""   
 
-    """
+"""
     The add_time and add_times method were removed because the AthleteList inherists from list which has all the methods of list including append (equivalent to add_time) and extend (equivalent to add_times)
-    """
+"""
 
     #def add_time(self, time_value):
         #self.times.append(time_value)
@@ -31,32 +52,6 @@ class Athlete:
     #def add_times(self, list_of_times):
         #self.times.extend(list_of_times)
 
-
-class AthleteList(list):
-    def __init__(self, a_name, a_dob=None, a_times=[]):
-            list.__init__([])
-            self.name = a_name
-            self.dob = a_dob
-            self.extend(a_times)
-            
-    def top3(self):
-            return(sorted(set([sanitize(t) for t in self]))[0:3])
-
-vera = Athlete('Vera Vi')
-vera.add_time('1.31')
-print(vera.top3())
-vera.add_times(['2.22', "1-21", '2:22'])
-print(vera.top3())
-
-def get_coach_data(filename):
-    try:
-        with open(filename, 'r') as f:
-            data = f.readline()
-        templ = data.strip().split(',')
-        return(Athlete(templ.pop(0),templ.pop(0), templ))
-    except IOError as ioerr:
-        print('File error: ' + str(ioerr))
-        return(None)
 
 james = get_coach_data('james2.txt')
 print(james.name + "'s fastest times are: " + str(james.top3()))
