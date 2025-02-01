0aa628468d8bee5a2a760455fe14182e6778f43b
Taofeek <bellotaophyc@outlook.com>
2025-02-01 16:58:20 +0100

    rewrite the class to inherit from the built-in list class

--- code_magnets/extract.py
+++ code_magnets/extract.py
@@ -19,12 +19,34 @@ class Athlete:
 
     def top3(self):
         return(sorted(set([sanitize(t) for t in self.times]))[0:3])
+    
 
-    def add_time(self, time_value):
-        self.times.append(time_value)
+    """
+    The add_time and add_times method were removed because the AthleteList inherists from list which has all the methods of list including append (equivalent to add_time) and extend (equivalent to add_times)
+    """
 
-    def add_times(self, list_of_times):
-        self.times.extend(list_of_times)
+    #def add_time(self, time_value):
+        #self.times.append(time_value)
+
+    #def add_times(self, list_of_times):
+        #self.times.extend(list_of_times)
+
+
+class AthleteList(list):
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+            list.__init__([])
+            self.name = a_name
+            self.dob = a_dob
+            self.extend(a_times)
+            
+    def top3(self):
+            return(sorted(set([sanitize(t) for t in self]))[0:3])
+
+vera = Athlete('Vera Vi')
+vera.add_time('1.31')
+print(vera.top3())
+vera.add_times(['2.22', "1-21", '2:22'])
+print(vera.top3())
 
 def get_coach_data(filename):
     try:
@@ -35,12 +57,6 @@ def get_coach_data(filename):
     except IOError as ioerr:
         print('File error: ' + str(ioerr))
         return(None)
-
-vera = Athlete('Vera Vi')
-vera.add_time('1.31')
-print(vera.top3())
-vera.add_times(['2.22', "1-21", '2:22'])
-print(vera.top3())
 
 james = get_coach_data('james2.txt')
 print(james.name + "'s fastest times are: " + str(james.top3()))
